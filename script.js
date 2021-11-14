a8102a69b8524dd97df6fc31a34728c1ec69bd28
s364549 <s364549@oslomet.no>
2021-11-14 20:14:58 +0100

    fun

--- script.js
+++ script.js
@@ -12,8 +12,8 @@ function randomN(min,max) {
 function change() {
   document.getElementById("capybara").src = "images/capy" + randomN(1,3) + ".png";
   
-  if (document.getElementById("counter").innerHTML > 99) {
-    document.getElementById("capybara").src = "images/capy" + 4 + ".png";
+  if (document.getElementById("counter").innerHTML > 49) {
+    document.getElementById("capybara").src = "images/capy" + randomN(4,6) + ".png";
   }
 }
 
