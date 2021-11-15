27e80d2e24ca019887961e3e4e42aad1249d6d78
s364549 <s364549@oslomet.no>
2021-11-15 16:29:27 +0100

    Fun
    Endret styling på fun for å oppfylle kravet om annerledes design

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 
-var image = document.getElementById("capybara").src;
-
 function countUp() {
   document.getElementById("counter").innerHTML++;
 }
@@ -9,9 +7,18 @@ function randomN(min,max) {
   return Math.floor(Math.random() * max) + min;
 }
 
+function showHide() {
+  var shownlist = document.getElementsByClassName("shown");
+  shownlist[0].style.display="none";
+  var hiddenlist = document.getElementsByClassName("hidden");
+  hiddenlist[0].style.display="inline-block";
+  hiddenlist[1].style.display="inline-block";
+}
+
 function change() {
-  if (document.getElementById("counter").innerHTML > 49) {
+  if (document.getElementById("counter").innerHTML > 20) {
     document.getElementById("capybara").src = "images/s-capy" + randomN(1,3) + ".png";
+    showHide();
   }
   else {
     document.getElementById("capybara").src = "images/capy" + randomN(1,3) + ".png";
